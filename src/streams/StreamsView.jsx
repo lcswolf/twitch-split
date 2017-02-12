0722ee9dc8c6f60a4694848629d47b6484185dd7
strothj <jason@jasons.io>
2017-02-11 19:08:04 -0500

    Added StreamsView route

---
+++ src/streams/StreamsView.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './StreamsView.scss';
+
+// {console.log(props.params.splat)}
+/* eslint-disable react/prop-types, no-console */
+export default () => (
+  <div>
+    Streams view placeholder: <br />
+  </div>
+);
---
+++ src/streams/index.js
@@ -0,0 +1,4 @@
+import StreamsView from './StreamsView';
+
+/* eslint-disable import/prefer-default-export */
+export { StreamsView };
